4b426f238bd74dcfebcab338fa75cddb518880e0
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-03-13 04:49:55 +0000

    Command descriptions added to each entry by Python script

--- python/opcode_org.py
+++ python/opcode_org.py
@@ -4,6 +4,62 @@ import csv
 
 class OpcodeHandler:
 
+    # Command descriptions
+    code_descriptions = {
+
+        'LD': 'Put',
+        'LDD': 'Put and decrement',
+        'LDI': 'Put and increment',
+        'LDH': 'Put in/out of memory address +0xFF00',
+        'LDHL': 'Put SP + address into HL',
+        'PUSH': 'Push register pair nn onto stack; Decrement Stack Pointer (SP) twice',
+        'POP': 'Pop two bytes off stack into register pair nn; Increment Stack Pointer (SP) twice',
+        'ADD': 'Add n to A',
+        'ADC': 'Add n + Carry flag to A',
+        'SUB': 'Subtract n from A',
+        'SBC': 'Subtract n + Carry flag from A',
+        'AND': 'Logically AND n with A, result in A',
+        'OR': 'Logical OR n with register A, result in A',
+        'XOR': 'Logical exclusive OR n with register A, result in A',
+        'CP': 'Compare A with n; This is basically an A - n subtraction instruction but the results are thrown away',
+        'INC': 'Increment register n',
+        'DEC': 'Decrement register n',
+        'SWAP': 'Swap upper & lower nibles of n',
+        'DAA': 'Decimal adjust register A; This instruction adjusts register A so that the correct \
+                representation of Binary Coded Decimal (BCD) is obtained',
+        'CPL': 'Complement A register; (flip all bits)',
+        'CCF': 'Complement carry flag; If C flag is set, then reset it; If C flag is reset, then set it',
+        'SCF': 'Set Carry flag',
+        'NOP': 'No operation',
+        'HALT': 'Power down CPU until an interrupt occurs; Use this when ever possible to reduce energy consumption',
+        'STOP': 'Halt CPU & LCD display until button pressed',
+        'DI': 'This instruction disables interrupts but not immediately; Interrupts are disabled after \
+                instruction after DI is executed',
+        'EI': 'Enable interrupts. This intruction enables interrupts but not immediately. Interrupts \
+                are enabled after instruction after EI is executed',
+        'RLCA': 'Rotate A left. Old bit 7 to Carry flag',
+        'RLA': 'Rotate A left through Carry flag',
+        'RRCA': 'Rotate A right; Old bit 0 to Carry flag',
+        'RRA': 'Rotate A right through Carry flag',
+        'RLC': 'Rotate n left; Old bit 7 to Carry flag',
+        'RL': 'Rotate n left through Carry flag',
+        'RRC': 'Rotate n right; Old bit 0 to Carry flag',
+        'RR': 'Rotate n right through Carry flag',
+        'SLA': 'Shift n left into Carry; LSB of n set to 0',
+        'SRA': 'Shift n right into Carry; MSB doesn`t change',
+        'SRL': 'Shift n right into Carry; MSB set to 0',
+        'BIT': 'Test bit b in register r',
+        'SET': 'Set bit b in register r',
+        'RES': 'Reset bit b in register r',
+        'JP': 'Jump to address nn; nn = two byte immediate value. (LS byte first); Conditional jump',
+        'JR': 'Add n to current address and jump to it; Conditional JR',
+        'CALL': 'Push address of next instruction onto stack and then jump to address nn; Conditional CALL',
+        'RST': 'Push present address onto stack; Jump to address $0000 + n',
+        'RET': 'Pop two bytes from stack & jump to that address; Conditional RET',
+        'RETI': 'Pop two bytes from stack & jump to that address then enable interrupts',
+
+    }
+
     def __init__(self, file_name):
         # Open CSV file containing opcode table
         self.input_file = open(file_name, newline='')
@@ -16,65 +72,12 @@ class OpcodeHandler:
         self.code_list = self.create_code_list()
         self.added_list = self.add_columns()
 
-        # Command descriptions
-        self.code_descriptions = {
 
-            'LD': 'Put',
-            'LDD': 'Put and decrement',
-            'LDI': 'Put and increment',
-            'LDH': 'Put in/out of memory address +0xFF00',
-            'LDHL': 'Put SP + address into HL',
-            'PUSH': 'Push register pair nn onto stack; Decrement Stack Pointer (SP) twice',
-            'POP': 'Pop two bytes off stack into register pair nn; Increment Stack Pointer (SP) twice',
-            'ADD': 'Add n to A',
-            'ADC': 'Add n + Carry flag to A',
-            'SUB': 'Subtract n from A',
-            'SBC': 'Subtract n + Carry flag from A',
-            'AND': 'Logically AND n with A, result in A',
-            'OR': 'Logical OR n with register A, result in A',
-            'XOR': 'Logical exclusive OR n with register A, result in A',
-            'CP': 'Compare A with n; This is basically an A - n subtraction instruction but the results are thrown away',
-            'INC': 'Increment register n',
-            'DEC': 'Decrement register n',
-            'SWAP': 'Swap upper & lower nibles of n',
-            'DAA': 'Decimal adjust register A; This instruction adjusts register A so that the correct \
-                    representation of Binary Coded Decimal (BCD) is obtained',
-            'CPL': 'Complement A register; (flip all bits)',
-            'CCF': 'Complement carry flag; If C flag is set, then reset it; If C flag is reset, then set it',
-            'SCF': 'Set Carry flag',
-            'NOP': 'No operation',
-            'HALT': 'Power down CPU until an interrupt occurs; Use this when ever possible to reduce energy consumption',
-            'STOP': 'Halt CPU & LCD display until button pressed',
-            'DI': 'This instruction disables interrupts but not immediately; Interrupts are disabled after \
-                    instruction after DI is executed',
-            'EI': 'Enable interrupts. This intruction enables interrupts but not immediately. Interrupts \
-                    are enabled after instruction after EI is executed',
-            'RLCA': 'Rotate A left. Old bit 7 to Carry flag',
-            'RLA': 'Rotate A left through Carry flag',
-            'RRCA': 'Rotate A right; Old bit 0 to Carry flag',
-            'RRA': 'Rotate A right through Carry flag',
-            'RLC': 'Rotate n left; Old bit 7 to Carry flag',
-            'RL': 'Rotate n left through Carry flag',
-            'RRC': 'Rotate n right; Old bit 0 to Carry flag',
-            'RR': 'Rotate n right through Carry flag',
-            'SLA': 'Shift n left into Carry; LSB of n set to 0',
-            'SRA': 'Shift n right into Carry; MSB doesn`t change',
-            'SRL': 'Shift n right into Carry; MSB set to 0',
-            'BIT': 'Test bit b in register r',
-            'SET': 'Set bit b in register r',
-            'RES': 'Reset bit b in register r',
-            'JP': 'Jump to address nn; nn = two byte immediate value. (LS byte first); Conditional jump',
-            'JR': 'Add n to current address and jump to it; Conditional JR',
-            'CALL': 'Push address of next instruction onto stack and then jump to address nn; Conditional CALL',
-            'RST': 'Push present address onto stack; Jump to address $0000 + n',
-            'RET': 'Pop two bytes from stack & jump to that address; Conditional RET',
-            'RETI': 'Pop two bytes from stack & jump to that address then enable interrupts',
-
-        }
 
     def create_code_table(self):
         op_table = []
         for row in self.csv_reader:
+            # Each row is a list, added to the op_table list
             op_table.append(row)
         return op_table
 
@@ -87,7 +90,6 @@ class OpcodeHandler:
             clean_table.append(clean_row)
         return clean_table
 
-
     def clean_num_cells(self):
 
         num_table = []
@@ -97,11 +99,10 @@ class OpcodeHandler:
         for i in range(len(self.clean_table[0])):
             num_cell = self.clean_table[0][i].replace(' ', '')
             num_cell = num_cell.replace('x', '')
-            #print(i)
             top_row.append(num_cell)
         num_table.append(top_row)
 
-        # Iterate through remaining rows
+        # Iterate through remaining rows and remove spaces and 'x' characters from first cell
         for i in range(1, len(self.clean_table)):
             num_row = []
             num_cell = self.clean_table[i][0].replace(' ', '')
@@ -138,7 +139,7 @@ class OpcodeHandler:
 
     def add_columns(self):
         added_list = []
-        #added_list.append(['code_no', 'bin_no', 'length', 'duration', 'Z', 'N', 'H', 'C'])
+        # added_list.append(['code_no', 'bin_no', 'length', 'duration', 'Z', 'N', 'H', 'C'])
 
         for i in range(len(self.code_list)):
 
@@ -200,6 +201,11 @@ class OpcodeHandler:
                     flag_columns = self.code_list[i][3].split(' ')
                     for k in flag_columns:
                         added_row.append(k)
+
+            try:
+                added_row.append(OpcodeHandler.code_descriptions[mnemonic])
+            except KeyError:
+                print('Command ' + mnemonic + ' not found.')
 
             added_list.append(added_row)
 
